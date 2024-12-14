01d6fd56506bb770839ea5603e31d824bd4b789b
myasoedas <myasoedas@yandex.ru>
2024-12-14 22:55:43 +0300

    Написал тест из 3-х проверок и создал шаблон главной страницы home.html

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,24 @@
 from django.test import TestCase
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_home_page_uses_correct_template(self):
+        """
+        Проверяем, что домашняя страница использует правильный шаблон.
+        """
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "lists/home.html")
+
+    def test_home_page_contains_correct_html(self):
+        """
+        Проверяем, что HTML содержит ожидаемый заголовок.
+        """
+        response = self.client.get("/")
+        self.assertContains(response, "<title>To-Do lists</title>")
+
+    def test_home_page_returns_http_200(self):
+        """
+        Проверяем, что сервер возвращает HTTP 200.
+        """
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    """
+    Обработчик для домашней страницы.
+    """
+    return render(request, "lists/home.html")
